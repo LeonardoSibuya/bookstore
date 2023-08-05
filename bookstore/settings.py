72d9c74dcd9daf7382885f28d7f00e609bf2e740
e_sibuya@outlook.com <*e_sibuya@outlook.com*>
2023-08-05 13:37:19 -0300

    corrigindo erro de identação

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -143,7 +143,7 @@ INTERNAL_IPS = [
     '127.0.0.1',
 ]
 
-REST_FRAMEWORK =  {
+REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 5,
     'DEFAULT_AUTHENTICATION_CLASSES': [
