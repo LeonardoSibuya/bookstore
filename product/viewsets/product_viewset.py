3efba0a56ec5be90caab585630a0a58b454ae376
e_sibuya@outlook.com <e_sibuya@outlook.com>
2023-11-06 12:03:20 -0300

    feat: build github actions

--- product/viewsets/product_viewset.py
+++ product/viewsets/product_viewset.py
@@ -2,10 +2,8 @@ from rest_framework.viewsets import ModelViewSet
 
 from product.models import Product
 from product.serializers.product_serializer import ProductSerializer 
-# from rest_framework.permissions import IsAuthenticated
 
 class ProductViewSet(ModelViewSet):
-    # permission_classes = [IsAuthenticated]
     serializer_class = ProductSerializer
 
     def get_queryset(self):
